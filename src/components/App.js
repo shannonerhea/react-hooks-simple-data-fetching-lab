8e44987123912abc1925cf51ce88ae416333813d
shannone <shannone426@gmail.com>
2022-09-06 11:19:21 -0400

    using fetch and useeffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, { useEffect, useState } from 'react'
+
+function  App() {
+    const [dogPic, setDogPic] = useState(null);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((resp) => resp.json())
+        .then((data) => {
+            setDogPic(data.message);
+        })
+    }, []);
+    if (!dogPic) return <p>Loading...</p>
+    return <img src={dogPic} alt=" A random dog pic" />
+};
+export default App;
